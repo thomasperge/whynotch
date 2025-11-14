8046518c838a10c7b7fe54e4ea1e31e5c38f5184
Thomasperge <thomaskauffmant74@gmail.com>
2025-11-14 21:50:34 +0100

    Initial Commit

---
+++ whynotch/whynotchApp.swift
@@ -0,0 +1,17 @@
+//
+//  whynotchApp.swift
+//  whynotch
+//
+//  Created by Thomas Kauffmant on 14/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct whynotchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ whynotchTests/whynotchTests.swift
@@ -0,0 +1,17 @@
+//
+//  whynotchTests.swift
+//  whynotchTests
+//
+//  Created by Thomas Kauffmant on 14/11/2025.
+//
+
+import Testing
+@testable import whynotch
+
+struct whynotchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
